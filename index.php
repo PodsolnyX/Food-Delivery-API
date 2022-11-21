d52f7583fbb05e77abbeb3ff76c48a6a1f63667f
Yuraev Aleksandr <podsolnyx03@mail.ru>
2022-11-21 15:36:15 +0700

    Refactoring index.php
    Extracting the router from index.php to the new file router.php

--- index.php
+++ index.php
@@ -1,57 +1,9 @@
 <?php
 
-    function getData($method) {
-        $data = new stdClass();
-
-        if ($method != "GET") {
-            $data->body = json_decode(file_get_contents('php://input'));
-        }
-        $data->parameters = [];
-            $dataGet = $_GET;
-            foreach ($dataGet as $key => $value) {
-                if ($key != "q") {
-                    $data->parameters[$key] = $value;
-                }
-            }
-        return $data;
-    }
-
-    function getMethod() {
-        return $_SERVER['REQUEST_METHOD'];
-    }
+    include_once 'scripts/router.php';
 
     header('Content-type: application/json');
-    include_once 'scripts/responses.php';
-
-    $link = mysqli_connect("127.0.0.1", "backend", "password", "deliveryFood");
-
-    if (!$link) {
-        echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-        echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Код ошибки error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }
     
-    $url = isset($_GET['q']) ? $_GET['q'] : '';
-    $url = rtrim($url, '/');
-    $urlList = explode('/', $url);
-
-    if ($urlList[0] == 'api') {
-
-        $router = $urlList[1];
-        $method = getMethod();
-        $requestData = getData($method);
-    
-        if (file_exists(realpath(dirname(__FILE__)).'/routers/' . $router . '.php')) {
-            include_once 'routers/' . $router . '.php';
-            route($method, $urlList, $requestData); 
-        }
-        else {
-            responseNotFound();
-        }
-    }
-    else {
-        responseNotFound();
-    }
+    routerDispatch();
 
 ?>
