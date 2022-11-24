0b81a0d787702f62e5a3376789869534c4decd26
Yuraev Aleksandr <podsolnyx03@mail.ru>
2022-11-25 00:11:05 +0700

    [setRating] Add setting raiting dish
    1)Add setting raiting dish without checking
    if this dish already has a rating from this user.
    2)Add checking whether the user has bought this dish.
    3)Add checking whether the token is valid.
    4)Replacing the plug in dish.php to the setRatingDish function.

--- routers/dish.php
+++ routers/dish.php
@@ -1,11 +1,13 @@
 <?php
 
+    include_once 'scripts/scriptsAPI/dishSetRating.php';
+
     function route($method, $urlList, $requestData) {
         include_once 'scripts/responses.php';
         switch ($method) {
             case 'POST':
                 if ($urlList[3] == 'rating' && $urlList[4] == null){
-                    echo 'api/dish/{id}/rating';
+                    setRatingDish($urlList[2], $requestData->body->ratingScore);
                 }
                 else {
                     responseNotFound();
---
+++ scripts/scriptsAPI/dishSetRating.php
@@ -0,0 +1,72 @@
+<?php
+
+    include_once 'scripts/responses.php';
+    include_once 'scripts/JWT.php';
+    include_once 'scripts/connectDB.php';
+
+    function setRatingDish($idDish, $ratingScore) {
+
+        if ($ratingScore < 0 && $ratingScore > 10) {
+            $response = [
+                "status" => '401',
+                "message" => 'Incorrect rating score'
+            ];
+            echo json_encode($response);
+            exit;
+        }
+
+        $link = connectToDataBase();
+
+        $dish = $link->query("SELECT * FROM dish WHERE idDish = '$idDish'")->fetch_assoc();
+
+        if ($dish == null) {
+            $response = [
+                "status" => '404',
+                "message" => 'Dish not found'
+            ];
+            echo json_encode($response);
+            exit;
+        }
+
+        $token = substr(getallheaders()['Authorization'], 7);
+
+        $result = $link->query("SELECT token FROM expired_token WHERE token = '$token'")->fetch_assoc();
+        
+        if (!isExpired($token) && isValid($token) && $result == null) {
+
+            $email = getPayload($token)["email"];
+
+            $resultUser = $link->query(
+            "SELECT user.idUser FROM user 
+            INNER JOIN dish_basket on user.idUser = dish_basket.idUser
+            WHERE email = '$email' AND idDish = '$idDish'")->fetch_assoc();
+
+            $idUser = $resultUser["idUser"];
+
+            if ($idUser != null) {
+                $result = $link->query(
+                    "INSERT INTO rating(idUser, idDish, rating) 
+                    VALUES ('$idUser', '$idDish', '$ratingScore')");
+                echo json_encode($link->error);
+            }
+            else {
+                $response = [
+                    "status" => '404',
+                    "message" => 'Пользователь не покупал этот товар'
+                ];
+                echo json_encode($response);
+                exit;
+            }
+
+        }
+        else {
+            $response = [
+                "status" => '401',
+                "message" => 'Unauthorized'
+            ];
+            echo json_encode($response);
+            exit;
+        }
+    }
+
+?>
