4fcac6fe72cfd4bc7407433aab755467cb2e04fe
Yuraev Aleksandr <podsolnyx03@mail.ru>
2022-11-26 13:22:10 +0700

    [createOrder]Add saving the order in the database
    1)Add saving the order in the database.
    2)Add checking the correctness of the delivery time.
    3)Add checking for an empty basket.

--- scripts/scriptsAPI/orderCreate.php
+++ scripts/scriptsAPI/orderCreate.php
@@ -21,41 +21,47 @@
                 WHERE email = '$email'")->fetch_assoc();
 
             $currentUser = $resultUser["idUser"];
+            $idOder = uniqid();
+            $orderPrice = 0;
+            $orderTime = new DateTime();
+            $orderTime = str_replace("T", " ", substr(gmdate(DATE_ATOM, $orderTime->getTimestamp()), 0, 19));
+            $deliveryTime = str_replace("T", " ", substr($requestData->body->deliveryTime, 0, 19));
+            $address = $requestData->body->address;
+
+            if ($orderTime >= $deliveryTime) {
+                $response = [
+                    "status" => '401',
+                    "message" => 'Время заказа позже, чем время доставки'
+                ];
+                echo json_encode($response);
+                exit; 
+            }
 
             $resultBasket = $link->query(
-                "SELECT dish.idDish, name, price, amount, image FROM dish_basket
+                "SELECT price, amount FROM dish_basket
                 INNER JOIN dish on dish_basket.idDish = dish.idDish
-                WHERE idUser = '$currentUser' AND idOrder IS NULL");
-    
-            $dishes = [];
-            $orderPrice = 0;
-            $nowTime = new DateTime();
-            $nowTime = $nowTime->getTimestamp();
+                WHERE idUser = '$currentUser' AND idOrder IS NULL"
+            );
     
             while ($row = mysqli_fetch_assoc($resultBasket)) {
                 $orderPrice += $row["price"]*$row["amount"];
-                $dishes[] = [
-                    "id" => $row["idDish"],
-                    "name" => $row["name"],
-                    "price" => $row["price"],
-                    "totalPrice" => $row["price"]*$row["amount"],
-                    "amount" => $row["amount"],
-                    "image" => $row["image"]
+            };
+
+            if ($orderPrice == 0) {
+                $response = [
+                    "status" => '404',
+                    "message" => 'Корзина пуста'
                 ];
+                echo json_encode($response);
+                exit;  
             }
 
-            $order = [
-                "id" => uniqid(),
-                "orderTime" => substr(gmdate(DATE_ATOM, $nowTime), 0, 19) . ".000Z",
-                "deliveryTime" => $requestData->body->deliveryTime,
-                "status" => "InProcess",
-                "price" => $orderPrice,
-                "dishes" => $dishes,
-                "address" => $requestData->body->address
-            ];
+            $result = $link->query(
+                "INSERT `order`(idOrder, idUser, deliveryTime, orderTime, status, price, address) 
+                VALUES ('$idOder', '$currentUser', '$deliveryTime', '$orderTime', 'InProcess', '$orderPrice', '$address')"
+            );
 
-            echo json_encode($order);
-    
+            $result = $link->query("UPDATE dish_basket SET idOrder = '$idOder' WHERE idOrder IS NULL");
         }
         else {
             $response = [
